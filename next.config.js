8cb4d9090e38b2a3cc5b2d6ad16521d7a26237fe
taylorjackson <taylor.jackson@me.com>
2023-11-11 14:49:56 -0800

    next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
+module.exports = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   output: 'export',
-  assetPrefix: './',
   images: { unoptimized: true },
   webpack: (config) => {
     config.resolve.alias.canvas = false
     return config
   },
 }
-
-export default nextConfig
