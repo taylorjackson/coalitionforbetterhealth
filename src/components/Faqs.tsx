eac1b1001cea801c0d0809978056964b3d2467bf
taylorjackson <taylor.jackson@me.com>
2023-11-14 20:04:53 -0800

    trigger a dev build

--- src/components/Faqs.tsx
+++ src/components/Faqs.tsx
@@ -115,7 +115,7 @@ export function Faqs() {
             <textarea
               name="message"
               id="message"
-              rows={2}
+              rows={1}
               className="block w-full rounded-md border-0 bg-white/50 px-3.5 py-2 text-black shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-[#9C27B0] sm:text-sm sm:leading-6"
               defaultValue={''}
             />
