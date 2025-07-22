0cd7af28c12318f107a9b8d7e90b98236823b6a0
taylorjackson <taylorjackson@bc1092225.ca>
2025-07-22 15:03:31 -0700

    wip

--- src/app/ourframework/page.tsx
+++ src/app/ourframework/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Location as ContactSection } from '@/components/Location'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import { CallToAction } from '@/components/CallToAction'
@@ -290,6 +291,8 @@ export default function AdvocacyPage() {
                     </div>
                 </div>
 
+                <CallToAction />
+
                 {/* Enablers section */}
                 <div className="relative py-24 sm:py-32">
                     {/* Reverse blob on left side */}
@@ -360,7 +363,20 @@ export default function AdvocacyPage() {
                 </div>
 
                 {/* Learning Health System section */}
-                <div className="py-24 sm:py-32">
+                <div className="relative py-24 sm:py-32">
+                    {/* Blob on right side */}
+                    <div
+                        className="absolute right-0 top-0 -z-10 -mr-24 transform-gpu overflow-hidden blur-3xl lg:mr-24 xl:mr-48"
+                        aria-hidden="true"
+                    >
+                        <div
+                            className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+                            style={{
+                                clipPath:
+                                    'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
+                            }}
+                        />
+                    </div>
                     <div className="mx-auto max-w-7xl px-6 lg:px-8">
                         <div className="mx-auto max-w-2xl lg:text-center">
                             <h2 className="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
@@ -388,9 +404,10 @@ export default function AdvocacyPage() {
                 </div>
 
                 {/* Call to action section */}
-                <CallToAction />
+
+                <ContactSection />
             </main>
-            <Footer showBorder={false} />
+            <Footer />
         </div>
     )
 }
--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -2,7 +2,7 @@ import { BuildingOffice2Icon, EnvelopeIcon } from '@heroicons/react/24/outline'
 
 export function Location() {
     return (
-        <div className="relative isolate bg-white" id="contact-us">
+        <div className="relative isolate" id="contact-us">
             <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
                 <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
                     <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
